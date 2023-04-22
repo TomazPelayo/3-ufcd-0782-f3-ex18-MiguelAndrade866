550ff252a0d6064506ff98fa77b814c196badf6f
MiguelAndrade866 <a31366@tomazpelayo.com>
2023-04-22 17:45:09 +0100

    exercicio18

--- LerDuasPalavras2.c
+++ LerDuasPalavras2.c
@@ -1,3 +1,14 @@
-// Resolva neste ficheiro o seu exercício
-
+#include <stdio.h>
+int main (){
+    char caractere1,caractere2,caractere3,caractere4,caractere5,caractere6;
+    caractere1 = getchar();
+    caractere2 = getchar(),
+    caractere3 = getchar();
+    printf("A primeira palavra é: %c%c%c\n",caractere1,caractere2,caractere3);
+    caractere4 = getchar();
+    caractere5 = getchar(),
+    caractere6 = getchar();
+    printf("A segunda palavra é : %c%c%c\n",caractere4,caractere5,caractere6);
+    return(0);
+}
 
